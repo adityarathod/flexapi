db11877aea636654cd95882fa0c0860b5d4b32a7
applecrazy <applecrazy@users.noreply.github.com>
2019-01-21 01:10:11 -0800

    Add compression and v1 routes

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var express = require('express');
 var cors = require('cors');
+var compression = require('compression')
 
 
 var versionZeroRouter = require('./routes/v0');
+var versionOneRouter = require('./routes/v1');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'doc')));
 app.use(cors());
 
 app.use('/', versionZeroRouter);
+app.use('/v1', versionOneRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
